03b31461b6e9069ebd037f3a85f9531dca011651
jqmeyers <33039680+jqmeyers@users.noreply.github.com>
2017-11-02 15:53:56 -0500

    Delete db.sh
    h

--- db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Change DBPASSWORD, DBUSER, DBHOST and DBNAME to match the values
-# your mysql_db_info file on the webdev server.
-mysql --password='Di20v-x3MfoC' --user='rjbauer' --host='dbdev.cs.uiowa.edu' 'db_rjbauer'
